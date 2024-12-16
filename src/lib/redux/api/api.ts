6b9feb6da614f9b7ad2b5de13fca5277997f9a00
Josh-Savvy <joshsavyy007@gmail.com>
2024-12-16 05:23:57 +0100

    feat: api route handlers

--- src/lib/redux/api/api.ts
+++ src/lib/redux/api/api.ts
@@ -1,10 +1,10 @@
 import { getAuthToken } from '@app/_utilities/helpers';
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
-const baseUrl = process.env.API_BASE_URL!;
+const baseUrl = process.env.NEXT_PUBLIC_API_BASE_URL!;
 
 export const prepareHeaders = (headers: Headers) => {
-  const apiKey = process.env.API_KEY!;
+  const apiKey = process.env.NEXT_PUBLIC_API_KEY!;
   headers.set('x-api-key', apiKey);
   const token = getAuthToken();
   if (token) headers.set('Authorization', `Bearer ${token}`);
